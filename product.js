7e67fd7fe9b84535608a28fc468a5403ab7371dd
Zzero202 <ahmedfaisaly2000@gmail.com>
2022-09-23 21:07:18 +0200

    editing some files

--- product.js
+++ product.js
@@ -1,76 +1,93 @@
 const product = [
     {
+        id:1,
         name: 'God of War',
         price: "50 EGP",
         img: "images/apple.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "mobile"
+        brand: "mobile",
+        desc : "",
     },
     {
+        id:2,
         name: 'Death Stranding',
         price: "250 EGP",
         img: "images/microsoft.jpg",
         quantity: "1",
-        add: " add to cart"
-        ,
-        brand: "mobile"
+        add: " add to cart",
+        brand: "mobile",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id : 3,
         name: 'The Last Of Us 2',
         price: "350 EGP",
         img: "images/hp.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "lap"
+        brand: "lap",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id : 4,
         name: 'God of War',
         price: "50 EGP",
         img: "images/apple.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "lap"
+        brand: "lap",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id : 5,
         name: 'Death Stranding',
         price: "250 EGP",
         img: "images/microsoft.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "lap"
+        brand: "lap",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id :6,
         name: 'The Last Of Us 2',
         price: "350 EGP",
         img: "images/hp.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "pc"
+        brand: "pc",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     }
     ,
     {
+        id :7,
         name: 'God of War',
         price: "50 EGP",
         img: "images/apple.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "pc"
+        brand: "pc",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id : 8,
         name: 'Death Stranding',
         price: "250 EGP",
         img: "images/microsoft.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "mobile"
+        brand: "mobile",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     },
     {
+        id:9,
         name: 'The Last Of Us 2',
         price: "350 EGP",
         img: "images/hp.jpg",
         quantity: "1",
         add: " add to cart",
-        brand: "pc"
+        brand: "pc",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
     }
 ];
