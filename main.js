a784965da5fe108c0940bafaacc34f85f119ed94
Ahmed Magdy <79977244+A7med-Ma9dy@users.noreply.github.com>
2022-09-24 23:50:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var result = '';
 // function to display products //
 function displayProducts() {
     product.forEach(function (e) {
-        result += '<div id=" ' + e.id + '" class="text-center col-md-4 store-product ' + e.brand + ' ">' + '<img id=" ' + e.id + '" src="' + e.img + '">' + '<p id=" ' + e.id + '">' + e.name + '</p>' + e.price + "<br><br>" + '<p> Brand :' + e.brand + "</p>" + '<p>Quantity : <input style="width:20%" type="number" value="' + e.quantity + '"></p>' + '<button class="btn btn-primary">' + e.add + '</button>' + '</div>'
+        result += '<div id=" ' + e.id + '" class="text-center col-md-4 store-product ' + e.brand + ' ">' + '<img style="cursor:pointer" id=" ' + e.id + '" src="' + e.img + '">' + '<p id=" ' + e.id + '">' + e.name + '</p>' + e.price + "<br><br>" + '<p> Brand :' + e.brand + "</p>" + '<p>Quantity : <input style="width:20%" type="number" value="' + e.quantity + '"></p>' + '<button class="btn btn-primary">' + e.add + '</button>' + '</div>'
     });
 
     document.getElementById("train").innerHTML = result;
