ac0d3140d27680fe887b751c5182f1e393c0cc70
hagargad <hagar-hosny-gad@hotmail.com>
2022-09-28 16:38:59 +0200

    cart

--- cart.js
+++ cart.js
@@ -1,201 +1,185 @@
-$(document).ready(function () {
-	var productItem = [
-		{
-			id: 1,
-			name: 'God of War',
-			price: "200 EGP",
-			img: "apple.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "mobile",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 2,
-			name: 'Death Stranding',
-			price: "100 EGP",
-			img: "microsoft.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "mobile",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
+let carts = document.querySelectorAll('.add-cart');
 
-		},
-		{
-			id: 3,
-			name: 'The Last Of Us 2',
-			price: "400 EGP",
-			img: "hp.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "lap",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 4,
-			name: 'God of War',
-			price: "350 EGP",
-			img: "apple.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "lap",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 5,
-			name: 'Death Stranding',
-			price: "150 EGP",
-			img: "microsoft.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "lap",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 6,
-			name: 'The Last Of Us 2',
-			price: "333 EGP",
-			img: "hp.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "pc",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		}
-		,
-		{
-			id: 7,
-			name: 'God of War',
-			price: "444 EGP",
-			img: "apple.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "pc",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 8,
-			name: 'Death Stranding',
-			price: "800 EGP",
-			img: "microsoft.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "mobile",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		},
-		{
-			id: 9,
-			name: 'The Last Of Us 2',
-			price: "350 EGP",
-			img: "hp.jpg",
-			quantity: "1",
-			// add: " add to cart",
-			brand: "pc",
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque amet quos sit obcaecati dolorem molestiae consectetur fuga, voluptas perferendis blanditiis iusto officia beatae iure vel veniam voluptatem assumenda veritatis, eum rerum saepe tempore pariatur ea! Suscipit nisi impedit expedita, soluta asperiores sapiente aperiam officia veniam iste doloribus sunt vitae.",
-		}
-
-	];
-	showProductGallery(productItem);
-	showCartTable();
-});
-
-function addToCart(element) {
-	var productParent = $(element).closest('div.product-item');
-
-	var price = $(productParent).find('.price span').text();
-	var productName = $(productParent).find('.productname').text();
-	var quantity = $(productParent).find('.product-quantity').val();
-
-	var cartItem = {
-		productName: productName,
-		price: price,
-		quantity: quantity
-	};
-	var cartItemJSON = JSON.stringify(cartItem);
-
-	var cartArray = new Array();
-	// If javascript shopping cart session is not empty
-	if (sessionStorage.getItem('shopping-cart')) {
-		cartArray = JSON.parse(sessionStorage.getItem('shopping-cart'));
-	}
-	cartArray.push(cartItemJSON);
-
-	var cartJSON = JSON.stringify(cartArray);
-	sessionStorage.setItem('shopping-cart', cartJSON);
-	window.location.href = 'cart.html';
-	showCartTable();
+for(let i=0; i< carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    });
 }
 
-function emptyCart() {
-	if (sessionStorage.getItem('shopping-cart')) {
-		// Clear JavaScript sessionStorage by index
-		sessionStorage.removeItem('shopping-cart');
-		showCartTable();
-	}
-	window.location.href = 'index.html';
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if( productNumbers ) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
 }
 
+function cartNumbers(product, action) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
 
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
 
-function removeCartItem(index) {
-	if (sessionStorage.getItem('shopping-cart')) {
-		var shoppingCart = JSON.parse(sessionStorage.getItem('shopping-cart'));
-		sessionStorage.removeItem(shoppingCart[index]);
-		showCartTable();
-	}
+    if( action ) {
+        localStorage.setItem("cartNumbers", productNumbers - 1);
+        document.querySelector('.cart span').textContent = productNumbers - 1;
+        console.log("action running");
+    } else if( productNumbers ) {
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem("cartNumbers", 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product);
 }
 
-function showCartTable() {
-	var cartRowHTML = "";
-	var itemCount = 0;
-	var grandTotal = 0;
-	var price = 0;
-	var quantity = 0;
-	var subTotal = 0;
+function setItems(product) {
+    // let productNumbers = localStorage.getItem('cartNumbers');
+    // productNumbers = parseInt(productNumbers);
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
 
-	if (sessionStorage.getItem('shopping-cart')) {
-		var shoppingCart = JSON.parse(sessionStorage.getItem('shopping-cart'));
-		itemCount = shoppingCart.length;
+    if(cartItems != null) {
+        let currentProduct = product.tag;
+    
+        if( cartItems[currentProduct] == undefined ) {
+            cartItems = {
+                ...cartItems,
+                [currentProduct]: product
+            }
+        } 
+        cartItems[currentProduct].inCart += 1;
 
-		//Iterate javascript shopping cart array
-		shoppingCart.forEach(function (item) {
-			var cartItem = JSON.parse(item);
-			price = parseFloat(cartItem.price);
-			quantity = parseInt(cartItem.quantity);
-			subTotal = price * quantity
+    } else {
+        product.inCart = 1;
+        cartItems = { 
+            [product.tag]: product
+        };
+    }
 
-			cartRowHTML += "<tr>" +
-				"<td>" + cartItem.productName + "</td>" +
-				"<td class='text-right'>£" + price.toFixed(2) + "</td>" +
-				"<td class='text-right'>" + quantity + "</td>" +
-				"<td class='text-right'>£" + subTotal.toFixed(2) + "</td>" +
-				"</tr>";
-
-			grandTotal += subTotal;
-		});
-	}
-
-	$('#cartTableBody').html(cartRowHTML);
-	$('#itemCount').text(itemCount);
-	$('#totalAmount').text("£" + grandTotal.toFixed(2));
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
 
+function totalCost( product, action ) {
+    let cart = localStorage.getItem("totalCost");
 
-function showProductGallery(product) {
-	//Iterate javascript shopping cart array
-	var productHTML = "";
-	product.forEach(function (item) {
-		productHTML += '<div class="product-item">' +
-			'<img src="images/' + item.img + '">' +
-			'<div class="productname">' + item.name + '</div>' +
-			'<div class="price">£<span>' + item.price + '</span></div>' +
-			'<div class="price">brand : <span>' + item.brand + '</span></div>' +
-			'<div class="cart-action" >' +
-			'<input type="text" class="product-quantity" name="quantity" value="1" size="2" />' +
-			'<input type="submit" value="Add to Cart" class="add-to-cart" onClick="addToCart(this)"  />' +
-			'</div>' +
-			'</div>';
-		"<tr>";
+    if( action) {
+        cart = parseInt(cart);
 
-	});
-	$('#product-item-container').html(productHTML);
+        localStorage.setItem("totalCost", cart - product.price);
+    } else if(cart != null) {
+        
+        cart = parseInt(cart);
+        localStorage.setItem("totalCost", cart + product.price);
+    
+    } else {
+        localStorage.setItem("totalCost", product.price);
+    }
 }
+
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    let cart = localStorage.getItem("totalCost");
+    cart = parseInt(cart);
+
+    let productContainer = document.querySelector('.products');
+    
+    if( cartItems && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map( (item, index) => {
+            productContainer.innerHTML += 
+            `<div class="product"><ion-icon name="close-circle"></ion-icon><img src="./images/${item.tag}.jpg" />
+                <span class="sm-hide">${item.name}</span>
+            </div>
+            <div class="price sm-hide">$${item.price},00</div>
+            <div class="quantity">
+                <ion-icon class="decrease " name="arrow-dropleft-circle"></ion-icon>
+                    <span>${item.inCart}</span>
+                <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>   
+            </div>
+            <div class="total">$${item.inCart * item.price},00</div>`;
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">Basket Total</h4>
+                <h4 class="basketTotal">$${cart},00</h4>
+            </div>`
+
+        deleteButtons();
+        manageQuantity();
+    }
+}
+
+function manageQuantity() {
+    let decreaseButtons = document.querySelectorAll('.decrease');
+    let increaseButtons = document.querySelectorAll('.increase');
+    let currentQuantity = 0;
+    let currentProduct = '';
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    for(let i=0; i < increaseButtons.length; i++) {
+        decreaseButtons[i].addEventListener('click', () => {
+            console.log(cartItems);
+            currentQuantity = decreaseButtons[i].parentElement.querySelector('span').textContent;
+            console.log(currentQuantity);
+            currentProduct = decreaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+            console.log(currentProduct);
+
+            if( cartItems[currentProduct].inCart > 1 ) {
+                cartItems[currentProduct].inCart -= 1;
+                cartNumbers(cartItems[currentProduct], "decrease");
+                totalCost(cartItems[currentProduct], "decrease");
+                localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+                displayCart();
+            }
+        });
+
+        increaseButtons[i].addEventListener('click', () => {
+            console.log(cartItems);
+            currentQuantity = increaseButtons[i].parentElement.querySelector('span').textContent;
+            console.log(currentQuantity);
+            currentProduct = increaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+            console.log(currentProduct);
+
+            cartItems[currentProduct].inCart += 1;
+            cartNumbers(cartItems[currentProduct]);
+            totalCost(cartItems[currentProduct]);
+            localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+            displayCart();
+        });
+    }
+}
+
+function deleteButtons() {
+    let deleteButtons = document.querySelectorAll('.product ion-icon');
+    let productNumbers = localStorage.getItem('cartNumbers');
+    let cartCost = localStorage.getItem("totalCost");
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    let productName;
+    console.log(cartItems);
+
+    for(let i=0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', () => {
+            productName = deleteButtons[i].parentElement.textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+           
+            localStorage.setItem('cartNumbers', productNumbers - cartItems[productName].inCart);
+            localStorage.setItem('totalCost', cartCost - ( cartItems[productName].price * cartItems[productName].inCart));
+
+            delete cartItems[productName];
+            localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+
+            displayCart();
+            onLoadCartNumbers();
+        })
+    }
+}
+
+onLoadCartNumbers();
+displayCart();
